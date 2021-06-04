0c50fa92958a07cd9c4768737366ff93474f9d99
Kakamotobi <79886384+Kakamotobi@users.noreply.github.com>
2021-06-04 17:14:29 +0900

    [JS] Reorganize Codes

--- app.js
+++ app.js
@@ -5,17 +5,7 @@ const hexBtn = document.querySelector("#hexBtn");
 const rgbBtn = document.querySelector("#rgbBtn");
 const hslBtn = document.querySelector("#hslBtn");
 
-// Generate Random Color
-let currRGB;
-const genRandomColor = () => {
-    const r = Math.floor(Math.random() * 255 + 1);
-    const g = Math.floor(Math.random() * 255 + 1);
-    const b = Math.floor(Math.random() * 255 + 1);
-    currRGB = [r, g, b];
-    return new Color(r, g, b);
-};
-
-// Event Listeners
+// ----------Event Listeners---------- //
 genColorBtn.addEventListener("click", function () {
     const newColor = genRandomColor();
     document.body.style.backgroundColor = newColor.rgb();
@@ -34,7 +24,9 @@ hslBtn.addEventListener("click", function () {
     colorName.innerText = new Color(...currRGB).hsl();
 });
 
-// Color Model Conversions
+copyBtn.addEventListener("click", copy);
+
+// ----------Color Class---------- //
 class Color {
     constructor(r, g, b) {
         this.r = r;
@@ -42,7 +34,6 @@ class Color {
         this.b = b;
         this.calcHSL();
     }
-    // Add methods to the prototype.
     hex() {
         const { r, g, b } = this;
         return (
@@ -57,15 +48,12 @@ class Color {
         const { h, s, l } = this;
         return `hsl(${h}, ${s}%, ${l}%)`;
     }
-    // For hsl
     calcHSL() {
         let { r, g, b } = this;
-        // Make r, g, and b fractions of 1
         r /= 255;
         g /= 255;
         b /= 255;
 
-        // Find greatest and smallest channel values
         let cmin = Math.min(r, g, b),
             cmax = Math.max(r, g, b),
             delta = cmax - cmin,
@@ -73,40 +61,35 @@ class Color {
             s = 0,
             l = 0;
         if (delta == 0) h = 0;
-        else if (cmax == r)
-            // Red is max
-            h = ((g - b) / delta) % 6;
-        else if (cmax == g)
-            // Green is max
-            h = (b - r) / delta + 2;
-        // Blue is max
+        else if (cmax == r) h = ((g - b) / delta) % 6;
+        else if (cmax == g) h = (b - r) / delta + 2;
         else h = (r - g) / delta + 4;
 
         h = Math.round(h * 60);
 
-        // Make negative hues positive behind 360°
         if (h < 0) h += 360;
-        // Calculate lightness
         l = (cmax + cmin) / 2;
 
-        // Calculate saturation
         s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
 
-        // Multiply l and s by 100
         s = +(s * 100).toFixed(1);
         l = +(l * 100).toFixed(1);
 
-        // Assign h, s, l to the object instance so that we can reuse it.
         this.h = h;
         this.s = s;
         this.l = l;
     }
 }
 
-// Copy to Clipboard
-copyBtn.addEventListener("click", copy);
-
-// Copy color
+// ----------Functions---------- //
+let currRGB;
+function genRandomColor() {
+    const r = Math.floor(Math.random() * 255 + 1);
+    const g = Math.floor(Math.random() * 255 + 1);
+    const b = Math.floor(Math.random() * 255 + 1);
+    currRGB = [r, g, b];
+    return new Color(r, g, b);
+}
 function copy() {
     const clipboard = navigator.clipboard;
     clipboard.writeText(colorName.innerText).then(() => {
